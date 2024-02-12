5a719af350caafd802069b5dd1d117be421f50e5
hasanasadov <125257868+hasanasadov@users.noreply.github.com>
2024-02-12 16:44:55 +0400

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -8,17 +8,15 @@ import sys
 import os
 import hashlib
 import re
-
-if len(sys.argv) < 3: 
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
-#salma
-markdown_file = sys.argv[1]
-output_file = sys.argv[2]
-
-if not os.path.exists(markdown_file):
-    print(f"Missing {markdown_file}", file=sys.stderr)
-    sys.exit(1)
-
-
-sys.exit(0)
+if __name__ == "__main__":
+    if len(sys.argv) < 3: 
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+    #salma
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+    
+    if not os.path.exists(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+    sys.exit(0)
